2fb18cbdf9a47f82febba1ca294cd7af64084db9
Taylor Skidmore <taylor@taylorskidmore.com>
2014-07-18 15:13:57 -0400

    salute command added

--- web.rb
+++ web.rb
@@ -11,21 +11,26 @@ end
 post '/' do
   msg = JSON.parse(request.body.read || '{"text": "something is super broke}')
   puts msg["text"]
-  # repeat(msg["text"])
+  if msg["text"].include?("o/")
+    salute()
+  end
 end
 
-def repeat(msg)
+def send_message(msg)
   uri = URI('https://api.groupme.com/v3/bots/post')
-  res_msg = {
-    "text"   => msg,
+  response_msg = {
+    "text" => msg,
     "bot_id" => "e8298ce556883d910629969317"
   }.to_json
-
+  
   gm_http = Net::HTTP.new(uri.host, uri.port)
   gm_http.use_ssl = true
   req = Net::HTTP::Post.new(uri.path, {'Content-Type' => 'application/json'})
-  req.body = res_msg
-
-  response = gm_http.request(req)
-  puts response.body
+  req.body = response_msg
+  gm_http.request(req)
 end
+
+def salute
+  send_message("\o")
+end
+
