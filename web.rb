f4d46f79e3cbd5db16f9f2c8de475483683f7b47
Taylor Skidmore <taylor@taylorskidmore.com>
2014-07-18 13:59:58 -0400

    annoying repeat

--- web.rb
+++ web.rb
@@ -1,9 +1,28 @@
 require 'sinatra'
+require 'json'
+require 'net/http'
 
 get '/' do
   "A group me bot!"
 end
 
 post '/' do
-  puts params
+  msg = JSON.parse(params)
+  repeat(msg)
+end
+
+def repeat(msg)
+  uri = URI('https://api.groupme.com/v3/bots/post')
+  res_msg = {
+    "text"   => msg["text"],
+    "bot_id" => "e8298ce556883d910629969317"
+  }.to_json
+
+  gm_http = Net::HTTP.new(uri.host, uri.port)
+  gm_http.use_ssl = true
+  req = Net::HTTP::Post.new(uri.path, {'Content-Type' => 'application/json'})
+  req.body = res_msg
+
+  response = gm_http.request(request)
+  puts response
 end
