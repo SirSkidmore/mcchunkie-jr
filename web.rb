77716272392e298c328f7a3eaf309deb1b761470
Taylor Skidmore <taylor@taylorskidmore.com>
2014-07-18 14:46:32 -0400

    annoying repeat GO

--- web.rb
+++ web.rb
@@ -2,19 +2,22 @@ require 'sinatra'
 require 'json'
 require 'net/http'
 
+set :server, "thin"
+
 get '/' do
   "A group me bot!"
 end
 
 post '/' do
-  msg = JSON.parse(params)
-  puts msg.text
+  msg = JSON.parse(request.body.read || '{"text": "something is super broke}')
+  puts msg["text"]
+  repeat(msg["text"])
 end
 
 def repeat(msg)
   uri = URI('https://api.groupme.com/v3/bots/post')
   res_msg = {
-    "text"   => msg["text"],
+    "text"   => msg,
     "bot_id" => "e8298ce556883d910629969317"
   }.to_json
 
@@ -23,6 +26,6 @@ def repeat(msg)
   req = Net::HTTP::Post.new(uri.path, {'Content-Type' => 'application/json'})
   req.body = res_msg
 
-  response = gm_http.request(request)
-  puts response
+  response = gm_http.request(req)
+  puts response.body
 end
